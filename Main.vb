50267735d8ed31e4f6096ab2d9ccbd424077ca15
sip-code <sip-code@outlook.com>
2020-10-17 14:31:49 -0400

    Add Progress Bar to data retrieval

--- Main.vb
+++ Main.vb
@@ -5,7 +5,7 @@ Public Class Main
     Private Property CurrentFile As String
 
     Private Sub GetFilingData(ByVal pstrFileName As String)
-        'Test 2
+
         lstExcelTab.SuspendLayout()
 
         Try
